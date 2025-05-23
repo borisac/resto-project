94bb1fe915ede8fb3131fb043c27b579888672c9
Borisa <borisa.cajic@gmail.com>
2025-05-23 20:02:07 +0200

    installing vue-router, creating routes. using router-view in App.vue, use of router via main.js

---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello, welcome to home page</h1>
+</template>
+<script>
+export default {
+    name:'Home'
+}
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -38,6 +38,7 @@ export default{
             if(result.status==201){
                 alert("sign up done");
                 localStorage.setItem("user-info", JSON.stringify(result.data));
+                this.$router.push({name:'Home'});
             }
         }
     }
