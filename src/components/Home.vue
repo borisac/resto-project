6723e63d2b5719df36866c13d17fdfa462be4f41
Borisa <borisa.cajic@gmail.com>
2025-05-23 22:41:21 +0200

    creating list view for GET rest method

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,56 @@
 <template>
     <Header></Header>
      <h1>Hello, welcome to home page</h1>
+     <table border="1">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Contact</td>
+            <td>Adress</td>
+            <td></td>
+        </tr>
+        <tr v-for="item in restaurant" :key="item.id">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.contact}}</td>
+            <td>{{item.adress}}</td>
+            <td></td>
+        </tr>
+     </table>
 </template>
 <script>
 
 import Header from './Header.vue'
+import axios from 'axios'
 
 export default {
     name:'Home',
     components:{
         Header
     },
-     mounted(){
+    data(){
+        return{
+            name:'',
+            restaurant:[],
+    }
+    },
+    async mounted(){
+
          let user= localStorage.getItem('user-info');
          if(!user){
             this.$router.push({name:'SignUp'});
          }
+
+          let result =await axios.get("http://localhost:3000/restaurants");
+          console.warn(result)
+          this.restaurant=result.data;
     },
 }
 </script>
+
+<style>
+td{
+    width:160px;
+    height:40px;
+}
+</style>
