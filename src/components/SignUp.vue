41138c104be5b1ca3cbe5cbab4fd15713169d0fb
Borisa <borisa.cajic@gmail.com>
2025-05-23 19:40:28 +0200

    Installing axios, adding signUp method, setting data to localStorage

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,18 +2,45 @@
     <img class="logo" src="../assets/resto-logo.jpg"/>
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter name">
-        <input type="text" placeholder="Enter email">
-        <input type="text" placeholder="Enter password">
-        <button>Sign Up</button>
-        
+        <input type="text" v-model="name" placeholder="Enter name">
+        <input type="text" v-model="email" placeholder="Enter email">
+        <input type="password" v-model="password" placeholder="Enter password">
+        <button v-on:click="signUp">Sign Up</button>
+
     </div>
 </template>
 
-<script>
+<script> 
+
+import axios from 'axios'
 
 export default{
+    
     name:'SignUp',
+
+    data(){
+        return{
+            name:'',
+            password:'',
+            email:''
+        }
+    },
+    methods:{
+        async signUp(){
+            let result = await axios.post("http://localhost:3000/users",{
+                    name:this.name,
+                    password:this.password,
+                    email:this.email
+            });
+            console.warn("TEST TEXT FOR SIGN UP", this.name,this.email,this.password);
+            console.warn("result promenljiva: ", result);
+
+            if(result.status==201){
+                alert("sign up done");
+                localStorage.setItem("user-info", JSON.stringify(result.data));
+            }
+        }
+    }
 
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// vue.config.js
+// vue.config.js 
 module.exports = {
     devServer: {
       port: 4700, // setup port
